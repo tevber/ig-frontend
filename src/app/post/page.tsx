faac69fdddf1b79f3b7ae0cea16a1209471f3b45
25HP5780 25HP5780 <25HP5780@G301-CC0195.local>
2025-10-04 09:59:33 +0800

    post page del

--- src/app/post/page.tsx
+++
@@ -1,6 +0,0 @@
-"use client";
-import { Button } from "@/components/ui/button";
-import { Input } from "@/components/ui/input";
-import { useUser } from "../../../../../instagram/instagram-frontend/src/providers/AuthProvider";
-import { useRouter } from "next/navigation";
-import { ChangeEvent, useState } from "react";
